78bf155e67759e9b9aa4645ee0270baeb665b762
Amandine SAHL <amandine.sahl@gmail.com>
2017-08-23 12:14:02 +0200

    Ajout du multilingue

--- src/pypnnomenclature/models.py
+++ src/pypnnomenclature/models.py
@@ -10,12 +10,22 @@ db = SQLAlchemy()
 
 class TNomenclatures(serializableModel, db.Model):
     __tablename__ = 't_nomenclatures'
-    __table_args__ = {'schema':'nomenclatures'}
+    __table_args__ = {'schema':'ref_nomenclatures'}
     id_nomenclature = db.Column(db.Integer, primary_key=True)
     id_type =  db.Column(db.Integer)
     mnemonique = db.Column(db.Unicode)
+    label_default = db.Column(db.Unicode)
+    definition_default = db.Column(db.Unicode)
     label_fr = db.Column(db.Unicode)
     definition_fr = db.Column(db.Unicode)
+    label_en = db.Column(db.Unicode)
+    definition_en = db.Column(db.Unicode)
+    label_es = db.Column(db.Unicode)
+    definition_es = db.Column(db.Unicode)
+    label_de = db.Column(db.Unicode)
+    definition_de = db.Column(db.Unicode)
+    label_it = db.Column(db.Unicode)
+    definition_it = db.Column(db.Unicode)
     source = db.Column(db.Unicode)
     statut = db.Column(db.Unicode)
     id_broader = db.Column(db.Integer)
@@ -27,29 +37,58 @@ class TNomenclatures(serializableModel, db.Model):
 
 class BibNomenclaturesTypes(serializableModel, db.Model):
     __tablename__ = 'bib_nomenclatures_types'
-    __table_args__ = {'schema':'nomenclatures'}
+    __table_args__ = {'schema':'ref_nomenclatures'}
     id_type = db.Column(db.Integer, primary_key=True)
     mnemonique = db.Column(db.Unicode)
+    label_default = db.Column(db.Unicode)
+    definition_default = db.Column(db.Unicode)
     label_fr = db.Column(db.Unicode)
     definition_fr = db.Column(db.Unicode)
+    label_en = db.Column(db.Unicode)
+    definition_en = db.Column(db.Unicode)
+    label_es = db.Column(db.Unicode)
+    definition_es = db.Column(db.Unicode)
+    label_de = db.Column(db.Unicode)
+    definition_de = db.Column(db.Unicode)
+    label_it = db.Column(db.Unicode)
+    definition_it = db.Column(db.Unicode)
     source = db.Column(db.Unicode)
     statut = db.Column(db.Unicode)
     meta_create_date = db.Column(db.DateTime)
     meta_update_date = db.Column(db.DateTime)
 
 
-
 class VNomenclatureTaxonomie(serializableModel, db.Model):
     __tablename__ = 'v_nomenclature_taxonomie'
-    __table_args__ = {'schema':'nomenclatures'}
+    __table_args__ = {'schema':'ref_nomenclatures'}
     id_type = db.Column(db.Integer)
-    type_nomenclature_label_fr = db.Column(db.Unicode)
-    type_nomenclature_definition_fr = db.Column(db.Unicode)
+    type_label = db.Column(db.Unicode)
+    type_definition = db.Column(db.Unicode)
+    type_label_fr = db.Column(db.Unicode)
+    type_definition_fr = db.Column(db.Unicode)
+    type_label_en = db.Column(db.Unicode)
+    type_definition_en = db.Column(db.Unicode)
+    type_label_es = db.Column(db.Unicode)
+    type_definition_es = db.Column(db.Unicode)
+    type_label_de = db.Column(db.Unicode)
+    type_definition_de = db.Column(db.Unicode)
+    type_label_it = db.Column(db.Unicode)
+    type_definition_it = db.Column(db.Unicode)
     regne = db.Column(db.Unicode, primary_key=True)
     group2_inpn = db.Column(db.Unicode, primary_key=True)
     id_nomenclature = db.Column(db.Integer, primary_key=True)
     mnemonique = db.Column(db.Unicode)
+    nomenclature_label = db.Column(db.Unicode)
+    nomenclature_definition = db.Column(db.Unicode)
     nomenclature_label_fr = db.Column(db.Unicode)
     nomenclature_definition_fr = db.Column(db.Unicode)
+    nomenclature_label_en = db.Column(db.Unicode)
+    nomenclature_definition_en = db.Column(db.Unicode)
+    nomenclature_label_es = db.Column(db.Unicode)
+    nomenclature_definition_es = db.Column(db.Unicode)
+    nomenclature_label_de = db.Column(db.Unicode)
+    nomenclature_definition_de = db.Column(db.Unicode)
+    nomenclature_label_it = db.Column(db.Unicode)
+    nomenclature_definition_it = db.Column(db.Unicode)
     id_broader = db.Column(db.Integer)
     hierarchy = db.Column(db.Unicode)
