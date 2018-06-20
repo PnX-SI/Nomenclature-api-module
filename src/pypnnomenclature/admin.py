131ceb0bcb136989149c23785474ece2f17300c9
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2018-06-20 16:42:15 +0200

    formatage

--- src/pypnnomenclature/admin.py
+++ src/pypnnomenclature/admin.py
@@ -11,14 +11,47 @@ from .models import (
 class TNomenclaturesAdmin(ModelView):
     page_size = 10
     column_searchable_list = ['id_type', 'mnemonique', 'id_nomenclature']
-    form_columns = ['id_nomenclature','nomenclature_type_name', 'cd_nomenclature', 'mnemonique', 'label_fr', 'definition_fr' , 'label_default', 'definition_default', 'statut', 'active', 'hierarchy', 'id_broader']
-    column_list = ['id_nomenclature','nomenclature_type_name', 'mnemonique','cd_nomenclature','label_default','definition_default', 'label_fr', 'definition_fr', 'id_type', 'statut', 'active']
+    form_columns = [
+      'id_nomenclature',
+      'nomenclature_type_name',
+      'cd_nomenclature',
+      'mnemonique',
+      'label_fr',
+      'definition_fr',
+      'label_default',
+      'definition_default',
+      'statut',
+      'active',
+      'hierarchy',
+      'id_broader'
+    ]
+    column_list = [
+      'id_nomenclature',
+      'nomenclature_type_name', 
+      'mnemonique',
+      'cd_nomenclature',
+      'label_default',
+      'definition_default',
+      'label_fr',
+      'definition_fr',
+      'id_type',
+      'statut',
+      'active'
+    ]
     page_size = 15    
 
 class BibNomenclaturesTypesAdmin(ModelView):
     page_size = 10
     column_searchable_list = ['label_default', 'id_type']
-    column_list = ['id_type', 'mnemonique', 'label_default', 'definition_default', 'label_fr', 'source', 'statut'  ]
+    column_list = [
+      'id_type',
+      'mnemonique',
+      'label_default',
+      'definition_default',
+      'label_fr',
+      'source',
+      'statut'
+    ]
     column_display_pk = True
     form_columns = [
       'id_type',
