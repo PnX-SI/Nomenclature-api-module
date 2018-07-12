070155521026d646805a2dcf7150c1b34f49ef73
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2018-07-12 09:12:31 +0200

    Script d'update

--- data/nomenclatures.sql
+++ data/nomenclatures.sql
@@ -170,6 +170,25 @@ $BODY$
   LANGUAGE plpgsql IMMUTABLE
   COST 100;
 
+CREATE OR REPLACE FUNCTION get_nomenclature_label_by_cdnom_mnemonique(
+    mytype character varying,
+    mycdnomenclature character varying)
+  RETURNS character varying AS
+$BODY$
+--Function which return the label from the id_nomenclature and the language
+DECLARE
+	labelfield character varying;
+	thelabel character varying;
+  BEGIN
+  EXECUTE format( ' SELECT  label_default
+  FROM ref_nomenclatures.t_nomenclatures n
+  WHERE cd_nomenclature = $1 AND id_type = ref_nomenclatures.get_id_nomenclature_type($2)' )INTO thelabel USING mycdnomenclature, mytype;
+return thelabel;
+  END;
+$BODY$
+  LANGUAGE plpgsql IMMUTABLE
+  COST 100;
+
 ----------
 --TABLES--
 ----------
