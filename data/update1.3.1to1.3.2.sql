9ef4c745071629b812dd5645b0460f59b6cad5db
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2019-12-30 10:16:03 +0100

    correction script migration

--- data/update1.3.1to1.3.2.sql
+++ data/update1.3.1to1.3.2.sql
@@ -2,10 +2,10 @@ UPDATE ref_nomenclatures.t_nomenclatures set mnemonique = 'sig'
 WHERE id_nomenclature = (
 SELECT id_nomenclature 
 FROM ref_nomenclatures.t_nomenclatures
-WHERE id_type = ref_nomenclatures.get_id_nomenclature_type('METHOD_CALCUL_SURFACE') AND cd_nomenclature = 'sig')
+WHERE id_type = ref_nomenclatures.get_id_nomenclature_type('METHOD_CALCUL_SURFACE') AND cd_nomenclature = 'sig');
 
 UPDATE ref_nomenclatures.t_nomenclatures set mnemonique = 'lin'
 WHERE id_nomenclature = (
 SELECT id_nomenclature 
 FROM ref_nomenclatures.t_nomenclatures
-WHERE id_type = ref_nomenclatures.get_id_nomenclature_type('METHOD_CALCUL_SURFACE') AND cd_nomenclature = 'lin')
+WHERE id_type = ref_nomenclatures.get_id_nomenclature_type('METHOD_CALCUL_SURFACE') AND cd_nomenclature = 'lin');
