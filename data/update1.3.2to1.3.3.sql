0d38d3c3c23d6e3fcffa73a5d33a9928e17fb84c
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2020-06-08 15:37:20 +0200

    correction date nomenclature #32

--- data/update1.3.2to1.3.3.sql
+++ data/update1.3.2to1.3.3.sql
@@ -1,10 +1,14 @@
-INSERT INTO ref_nomenclatures.bib_nomenclatures_types (mnemonique, label_fr, label_default, definition_fr, source, statut, meta_create_date, meta_update_date) VALUES
-('JDD_DATA_TYPE', 'Type de jeu de donnéees', 'Type de jeu de donnéees', 'Nomenclature des types de jeux de données', 'SINP', 'Validé', '2020-03-21 00:00:00', '2020-03-21 00:00:00');
+UPDATE ref_nomenclatures.t_nomenclatures 
+SET cd_nomenclature = concat('OLD_', cd_nomenclature)
+WHERE id_type = ref_nomenclatures.get_id_nomenclature_type('DATA_TYP');
+
 
 INSERT INTO ref_nomenclatures.t_nomenclatures (id_type, cd_nomenclature, mnemonique, label_fr, label_default, definition_fr,  source, statut, id_broader, meta_create_date, meta_update_date, active) VALUES
-(ref_nomenclatures.get_id_nomenclature_type('JDD_DATA_TYPE'), 'NR', 'NR', 'Non renseigné', 'Non renseigné', 'Le type de JDD n''est pas renseigné', 'SINP', 'Validé', 0, '2020-03-21 00:00:00', '2020-03-21 00:00:00', true)
-,(ref_nomenclatures.get_id_nomenclature_type('JDD_DATA_TYPE'), 'Hab', 'Hab', 'Habitats', 'Habitats', 'Habitats', 'SINP', 'Validé', 0, '2020-03-21 00:00:00', '2020-03-21 00:00:00', true)
-,(ref_nomenclatures.get_id_nomenclature_type('JDD_DATA_TYPE'), 'Tax', 'Tax', 'Taxons', 'Taxons', 'Taxons', 'SINP', 'Validé', 0, '2020-03-21 00:00:00', '2020-03-21 00:00:00', true);
+(ref_nomenclatures.get_id_nomenclature_type('DATA_TYP'), 'NR', 'NR', 'Non renseigné', 'Non renseigné', 'Le type de JDD n''est pas renseigné', 'SINP', 'Validé', 0, '2017-12-18 00:00:00', '2017-12-18 00:00:00', true)
+,(ref_nomenclatures.get_id_nomenclature_type('DATA_TYP'), 'Hab', 'Hab', 'Habitats', 'Habitats', 'Habitats', 'SINP', 'Validé', 0, '2017-12-18 00:00:00', '2017-12-18 00:00:00', true)
+,(ref_nomenclatures.get_id_nomenclature_type('DATA_TYP'), 'Tax', 'Tax', 'Taxons', 'Taxons', 'Taxons', 'SINP', 'Validé', 0, '2017-12-18 00:00:00', '2017-12-18 00:00:00', true);
 
+DELETE FROM ref_nomenclatures.defaults_nomenclatures_value
+WHERE mnemonique_type = 'DATA_TYP';
 INSERT INTO ref_nomenclatures.defaults_nomenclatures_value (mnemonique_type, id_organism, id_nomenclature) VALUES
-('JDD_DATA_TYPE',0,ref_nomenclatures.get_id_nomenclature('JDD_DATA_TYPE', 'NR'));
+('DATA_TYP',0,ref_nomenclatures.get_id_nomenclature('DATA_TYP', 'NR'));
