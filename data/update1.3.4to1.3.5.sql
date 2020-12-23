d8fba8a8f7ec4141b954cda45ce5b899fdc32fa5
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2020-12-23 16:58:52 +0100

    update sensi label

--- data/update1.3.4to1.3.5.sql
+++ data/update1.3.4to1.3.5.sql
@@ -3,3 +3,24 @@ INSERT INTO ref_nomenclatures.t_nomenclatures (id_type, cd_nomenclature, mnemoni
 
 INSERT INTO ref_nomenclatures.cor_taxref_nomenclature VALUES
 (ref_nomenclatures.get_id_nomenclature('STATUT_OBS', 'NSP'), 'all', 'all', now(), NULL);
+
+
+update ref_nomenclatures.t_nomenclatures 
+set label_fr = 'Non sensible'
+where id_type = (select id_type from ref_nomenclatures.bib_nomenclatures_types where mnemonique = 'SENSIBILITE') and cd_nomenclature = '0';
+
+update ref_nomenclatures.t_nomenclatures 
+set label_fr = 'Commune ou Znieff (visible uniquement à partir du niveau)'
+where id_type = (select id_type from ref_nomenclatures.bib_nomenclatures_types where mnemonique = 'SENSIBILITE') and cd_nomenclature = '1';
+
+update ref_nomenclatures.t_nomenclatures 
+set label_fr = 'Mailles 10 (visible uniquement à partir du niveau)'
+where id_type = (select id_type from ref_nomenclatures.bib_nomenclatures_types where mnemonique = 'SENSIBILITE') and cd_nomenclature = '2';
+
+update ref_nomenclatures.t_nomenclatures 
+set label_fr = 'Départements (visible uniquement à partir du niveau)'
+where id_type = (select id_type from ref_nomenclatures.bib_nomenclatures_types where mnemonique = 'SENSIBILITE') and cd_nomenclature = '3';
+
+update ref_nomenclatures.t_nomenclatures 
+set label_fr = 'Aucune diffusion'
+where id_type = (select id_type from ref_nomenclatures.bib_nomenclatures_types where mnemonique = 'SENSIBILITE') and cd_nomenclature = '4';
