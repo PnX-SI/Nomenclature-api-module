49180cf3a9ae83c1d7967ed52ea1565a4dfa4280
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2021-02-16 12:15:03 +0100

    fix migration script

--- data/update1.3.4to1.3.5.sql
+++ data/update1.3.4to1.3.5.sql
@@ -4,12 +4,13 @@ BEGIN
     IF EXISTS (select * from ref_nomenclatures.t_nomenclatures where cd_nomenclature = 'NSP' and id_type=ref_nomenclatures.get_id_nomenclature_type('STATUT_OBS'))
     THEN
     UPDATE ref_nomenclatures.t_nomenclatures SET active = true WHERE id_nomenclature = ref_nomenclatures.get_id_nomenclature('STATUT_OBS', 'NSP');
-
     ELSE
     INSERT INTO ref_nomenclatures.t_nomenclatures (id_type, cd_nomenclature, mnemonique, label_fr, label_default, definition_fr, source, statut, id_broader, hierarchy, meta_create_date, meta_update_date, active) VALUES
     (ref_nomenclatures.get_id_nomenclature_type('STATUT_OBS'), 'NSP', 'NSP', 'Ne Sait Pas', 'Ne Sait Pas', 'Ne Sait Pas : l''information n''est pas connue', 'SINP', 'Validé', 0, '018.003', '2020-12-22 00:00:00', '2020-12-22 00:00:00', true);
     END IF;
-
+END $$;
+DO $$
+BEGIN
     IF EXISTS(
         SELECT * FROM ref_nomenclatures.cor_taxref_nomenclature WHERE 
         id_nomenclature = ref_nomenclatures.get_id_nomenclature('STATUT_OBS', 'NSP') AND 
