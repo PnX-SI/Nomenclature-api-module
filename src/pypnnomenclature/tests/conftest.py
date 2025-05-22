758e9a47a3e1157e75195d3d82aaef14c7b16f10
Pierre-Narcisi <pierre.narcisi@epitech.eu>
2025-05-22 17:26:06 +0200

    prepare release

--- src/pypnnomenclature/tests/conftest.py
+++ src/pypnnomenclature/tests/conftest.py
@@ -1,14 +1,30 @@
 import pytest
+from flask import request
+from utils_flask_sqla.tests.utils import JSONClient
 
 from pypnnomenclature import create_app
 from pypnnomenclature.env import db
 
 
 @pytest.fixture(scope="session")
-def app():
-    return create_app()
+def _app():
+    app = create_app()
+    app.testing = True
+    app.test_client_class = JSONClient
+
+    @app.before_request
+    def get_endpoint():
+        pytest.endpoint = request.endpoint
+
+    with app.app_context():
+        yield app
 
 
 @pytest.fixture(scope="session")
-def _session(app):
+def _session(_app):
     return db.session
+
+
+@pytest.fixture(scope="session", autouse=True)
+def app(_app, _session):
+    return _app
